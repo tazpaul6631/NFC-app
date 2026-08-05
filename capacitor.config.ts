abfc651e79ae96f2a2482ba6cefede2440c8f571
JIAHSIN\Thuan.Truong <tazpaul6631@gmail.com>
2026-08-05 11:28:31 +0700

    change auto build apk

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -28,11 +28,11 @@ const config: CapacitorConfig = {
       style: 'LIGHT',
     },
   },
-  server: {
-    url: 'http://10.0.149.28:8100',
-    cleartext: true, // Cho phép chạy HTTP (không cần HTTPS)
-    allowNavigation: ['10.0.149.28']
-  }
+  // server: {
+  //   url: 'http://10.0.149.28:8100',
+  //   cleartext: true, // Cho phép chạy HTTP (không cần HTTPS)
+  //   allowNavigation: ['10.0.149.28']
+  // }
 }
 
 export default config
--- src/api/baseURLApi.ts
+++ src/api/baseURLApi.ts
@@ -3,5 +3,5 @@ const urlServer = 'https://ap02.jiahsin.com.vn/api';
 const urlLocalWifi = 'http://10.0.149.107:7152';
 
 export default {
-  url: urlLocal
+  url: urlServer
 };
