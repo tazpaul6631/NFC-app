6911a4c475129a77bb2ccbce8e0d58eb2d180130
Thuan <tazpaul6631@gmail.com>
2026-08-09 10:32:32 +0700

    change api

--- src/api/baseURLApi.ts
+++ src/api/baseURLApi.ts
@@ -1,5 +1,5 @@
 const urlLocal = 'http://10.0.111.118:7253';
-const urlServer = 'https://ap02.jiahsin.com.vn/api';
+const urlServer = 'https://BusCheckIn.jiahsin.com.vn/Api';
 const urlLocalWifi = 'http://10.0.149.107:7152';
 
 export default {
